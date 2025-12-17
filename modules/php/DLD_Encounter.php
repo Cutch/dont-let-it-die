d4ad7ab86b6a4fb9ca5a5f0274c3e519c47c6680
Cutch <onlycutch@hotmail.com>
2025-12-17 06:14:04 -0500

    Fix sling shot use cost

--- modules/php/DLD_Encounter.php
+++ modules/php/DLD_Encounter.php
@@ -104,7 +104,7 @@ class DLD_Encounter
 
         if (array_key_exists('useCost', $selectedWeapon)) {
             foreach ($selectedWeapon['useCost'] as $key => $value) {
-                if ($this->game->adjustResource($key, -$value)['left'] > 0) {
+                if ($this->game->adjustResource($key, -$value)['left'] != 0) {
                     throw new BgaUserException(clienttranslate('Missing resources'));
                 }
             }
